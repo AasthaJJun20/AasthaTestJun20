7fafb220c0a3b1b1bd53030e29b5175e3b513f96
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-08-06 23:05:35 +0530

    AasthaTestJun20_Branch2_Test3and4 file

---
+++ src/P2/Test3.java
@@ -0,0 +1,7 @@
+package P2;
+
+public class Test3 {
+	public static void main(String[] args) {
+		System.out.println("Branch2-Test3 file");
+	}
+}
---
+++ src/P2/Test4.java
@@ -0,0 +1,7 @@
+package P2;
+
+public class Test4 {
+	public static void main(String[] args) {
+		System.out.println("AasthaTestJun20-Branch2-Test4 file");
+	}
+}
