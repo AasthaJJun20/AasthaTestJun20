58a039b25f5ec62baa9977f280d33890426b79d4
Aastha Jaiswal <jaiswal.aastha1691@gmail.com>
2020-08-07 17:59:34 +0530

    Adding changes in Test7 file from branch A1

--- src/P2/Test7.java
+++ src/P2/Test7.java
@@ -3,5 +3,6 @@ package P2;
 public class Test7 {
 	public static void main(String[] args) {
 		System.out.println("AasthaTestJun20-Remote branch-Test7 file");
+		System.out.println("Test Conflict_- Changes from BrachA1");
 	}
 }
